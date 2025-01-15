ed4714f2271ece06f3af43866aaa0be3b0fa0d69
Luis Hidalgo <hid21002@byui.com>
2025-01-15 12:45:48 -0500

    updates

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,22 +12,6 @@ export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_P
 
 
 
-
-export const fire = {
-    firebase: {
-        apiKey: "AIzaSyC9aVxAL1jurkeppPhV0baXN-gWpDMGZV0",
-        authDomain: "node-portfolio-e56c3.firebaseapp.com",
-        projectId: "node-portfolio-e56c3",
-        storageBucket: "node-portfolio-e56c3.firebasestorage.app",
-        messagingSenderId: "422218756595",
-        appId: "1:422218756595:web:4d74a5ccba6784ef2c6286",
-        measurementId: "G-J53707ZWJL"
-    }
-    
-}
-
-
-
 export const server = {
     SERVER_HOSTNAME,
     SERVER_PORT
